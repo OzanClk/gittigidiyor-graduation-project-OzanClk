7103e537ed255aa86252ec45c50b3dfd110bf570
OzanClk <ozancelikinfo@gmail.com>
2021-09-24 12:51:55 +0300

    credit score service initiation

---
+++ credit-score-service/src/test/java/dev/patika/creditscore/CreditScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.creditscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditScoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
