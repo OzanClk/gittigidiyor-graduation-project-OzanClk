593b5ec3c31b5630409e4789d635743c811d1d29
OzanClk <ozancelikinfo@gmail.com>
2021-09-25 15:50:19 +0300

    frontend login service initiation. React and dependecies added.

---
+++ frontend-login-service/src/test/java/dev/patika/frontendlogin/FrontEndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.frontendlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
